84d4c52d1b4b92769fc70d56a5265834fb033263
Joshua Partogi <joshua.partogi@gmail.com>
2011-02-10 08:34:34 +1100

    Refactor model

--- lib/cassandrb/model.rb
+++ lib/cassandrb/model.rb
@@ -3,13 +3,32 @@ module Cassandrb
     extend ActiveSupport::Concern
     extend ActiveSupport::Autoload
 
+    autoload :Key
+    autoload :Finders
+    autoload :Persistence
+    autoload :ColumnFamily
+
     included do
       extend ActiveModel::Naming
       include ActiveModel::Serialization
       extend Cassandrb::Columns
       include Cassandrb::AttributeMethods
+      include Cassandrb::Model::Key
+      include Cassandrb::Model::Finders
+      include Cassandrb::Model::Persistence
+      include Cassandrb::Model::ColumnFamily
     end
 
-    
+    module InstanceMethods
+      def client
+        self.class.client
+      end
+    end
+
+    module ClassMethods
+      def client
+        Cassandrb.client
+      end
+    end
   end
 end
