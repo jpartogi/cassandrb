76e689acd6b3bdc2e457bbbe28ab4a7e81c47a9f
Joshua Partogi <joshua.partogi@gmail.com>
2011-02-10 21:19:16 +1100

    Finish off find

--- lib/cassandrb/model/finders.rb
+++ lib/cassandrb/model/finders.rb
@@ -5,7 +5,17 @@ module Cassandrb
 
       module ClassMethods
         def find(key)
-          self.client.get(self.column_family, key)
+          instantiate(self.client.get(self.column_family, key))
+        end
+
+        private
+        def instantiate(ordered_hash)
+          clazz= self.model_name.constantize rescue self
+          
+          hash= ordered_hash.to_hash
+          clazz.new.tap do |model|
+            hash.each {|k,v| model.send("#{k}=", v) }
+          end
         end
       end
     end
--- spec/finders_spec.rb
+++ spec/finders_spec.rb
@@ -4,10 +4,23 @@ require 'spec_helper'
 class Person
   include Cassandrb::Model
 
+  column :name
 end
 
 describe Cassandrb::Model::Finders do
+  before :all do
+    @client = Cassandra.new('Keyspace1')
+  end
+
+  after :all do
+    @client.disconnect!
+  end
+
   it "should get model by key" do
+    Cassandrb.client= @client
+
+    # Data inserted from persistence_spec
     person = Person.find('1')
+    person.name.should == 'Joe'
   end
 end
