c2b6be24cc595d17bbde59d7b5470018fc421eb3
Joshua Partogi <joshua.partogi@gmail.com>
2011-02-11 22:32:09 +1100

    Add update and destroy

--- lib/cassandrb/model/persistence.rb
+++ lib/cassandrb/model/persistence.rb
@@ -4,16 +4,29 @@ module Cassandrb
       extend ActiveSupport::Concern
       extend ActiveSupport::Autoload
 
+      module ClassMethods      
+        def create(attrs={})
+          self.new(attrs).tap {|o| o.save }
+        end
+      end
 
       module InstanceMethods
-        def save(*args)
-          self.key= SimpleUUID::UUID.new.to_s if self.key.nil?
-          self.client.insert(self.column_family, self.key, self.attributes)
+        def save(options={})
+          self.key= SimpleUUID::UUID.new.to_guid if self.key.nil?
+          self.client.insert(self.column_family, self.key, self.attributes, options)
           true
         end
 
-        def destroy
-          
+        def destroy(options={})
+          self.client.remove(self.column_family, self.key, options)
+          true
+        end
+        alias delete destroy
+        alias remove destroy
+
+        def update_attributes(attrs, options={})
+          self.client.insert(self.column_family, self.key, attrs, options)
+          true
         end
       end
     end
--- spec/persistence_spec.rb
+++ spec/persistence_spec.rb
@@ -16,9 +16,35 @@ describe Cassandrb::Model::Persistence do
     @client.disconnect!
   end
 
-  it "should save model" do
+  it "should be able to save model" do
     person = Person.new(:name => 'Joe')
     person.key= "1"
     person.save.should be true
+
+    person = Person.create(:name => 'Boe')
+    person.should_not be nil
+  end
+
+  it "should be able to destroy model" do
+    person = Person.new(:name => 'Moe')
+    person.key= "3"
+    person.save
+
+    person = Person.find("3")
+    person.destroy.should be true
+
+    person = Person.find("3")
+    person.should be nil
+  end
+
+  it "should be able to update model" do
+    person = Person.new(:name => 'Doe')
+    person.key= "4"
+    person.save
+
+    person.update_attributes(:name => 'Zoe')
+
+    person = Person.find("4")
+    person.name.should == "Zoe"
   end
 end
